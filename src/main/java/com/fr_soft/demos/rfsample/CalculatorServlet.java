821a7e848f50009d9120511fd8883572e9c752f3
Takuo Doi <doi@fr-soft.com>
2013-01-18 05:50:00 +0900

    Implemented CalculatorServlet.

---
+++ src/main/java/com/fr_soft/demos/rfsample/CalculatorServlet.java
@@ -0,0 +1,38 @@
+package com.fr_soft.demos.rfsample;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class CalculatorServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 6961400581681209885L;
+
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getRequestDispatcher("calculator.jsp").forward(request, response);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)	throws ServletException, IOException {
+	    String arg1 = request.getParameter("arg1");
+        request.setAttribute("arg1", arg1);
+	    String arg2 = request.getParameter("arg2");
+        request.setAttribute("arg2", arg2);
+
+        try {
+            Integer v1 = Integer.parseInt(arg1);
+            Integer v2 = Integer.parseInt(arg2);
+	        int value = v1.intValue() + v2.intValue();
+	        request.setAttribute("result", String.valueOf(value));
+	    } catch (NumberFormatException e) {
+	        request.setAttribute("result", "N/A");	        
+	    }
+        
+        request.getRequestDispatcher("calculator.jsp").forward(request, response);
+	}
+	
+}
---
+++ src/test/java/com/fr_soft/demos/rfsample/CalculatorServletTest.java
@@ -0,0 +1,63 @@
+package com.fr_soft.demos.rfsample;
+
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class CalculatorServletTest {
+    
+    private CalculatorServlet servlet = new CalculatorServlet();
+    private MockHttpServletRequest request = new MockHttpServletRequest();
+    private MockHttpServletResponse response = new MockHttpServletResponse();
+
+    @Test
+    public void test_doGetShouldReturnsNoArgsAndResult() throws IOException, ServletException {
+        this.servlet.doGet(this.request, this.response);
+
+        assertNull(this.request.getAttribute("arg1"));
+        assertNull(this.request.getAttribute("arg2"));
+        assertNull(this.request.getAttribute("result"));
+    }
+    
+    @Test
+    public void test_doPostReturnsErrorMessageIfArg1IsNotNumber() throws IOException, ServletException {
+        this.request.addParameter("arg1", "XYZ");
+        this.request.addParameter("arg2", "100");
+ 
+        this.servlet.doPost(this.request, this.response);
+
+        assertEquals("XYZ", this.request.getAttribute("arg1"));
+        assertEquals("100", this.request.getAttribute("arg2"));
+        assertEquals("N/A", this.request.getAttribute("result"));
+    }
+
+    @Test
+    public void test_doPostReturnsErrorMessageIfArg2IsNotNumber() throws IOException, ServletException {
+        this.request.addParameter("arg1", "ABC");
+        this.request.addParameter("arg2", "200");
+
+        this.servlet.doPost(this.request, this.response);
+
+        assertEquals("ABC", this.request.getAttribute("arg1"));
+        assertEquals("200", this.request.getAttribute("arg2"));
+        assertEquals("N/A", this.request.getAttribute("result"));
+    }
+
+    @Test
+    public void test_doPostReturnsAnswerIfBothArgsAreNumber() throws IOException, ServletException {
+        this.request.addParameter("arg1", "100");
+        this.request.addParameter("arg2", "200");
+
+        this.servlet.doPost(this.request, this.response);
+
+        assertEquals("100", this.request.getAttribute("arg1"));
+        assertEquals("200", this.request.getAttribute("arg2"));
+        assertEquals("300", this.request.getAttribute("result"));
+    }
+}
